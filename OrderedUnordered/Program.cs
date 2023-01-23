250c2ecb4fe474cc9abb290c02342ff70ad6df3d
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-23 22:09:55 +0530

    Added Ordered List To The Linked List Program

--- OrderedUnordered/Program.cs
+++ OrderedUnordered/Program.cs
@@ -5,6 +5,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Display Ordered And Unorderd Linkedlist");
+
+            OrderedList orderList = new OrderedList();
+
+            Console.WriteLine("Enter word to search");
+            string word = Console.ReadLine();
+            orderList.Ordered(word);
         }
     }
 }
