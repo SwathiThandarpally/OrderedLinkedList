0dee70f58b557817de81185de9ea26b6e7c93f72
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-23 22:00:03 +0530

    [Swathi]:Added Ordered Unordered List in Linked List Program

---
+++ OrderedUnordered/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderedUnordered
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Display Ordered And Unorderd Linkedlist");
+        }
+    }
+}
