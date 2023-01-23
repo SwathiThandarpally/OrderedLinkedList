2f1edc5db177e3d9070216c9647df700d6be48de
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-23 22:21:52 +0530

    Added Unordered List To LinkedList Program

--- OrderedUnordered/Program.cs
+++ OrderedUnordered/Program.cs
@@ -5,12 +5,17 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Display Ordered And Unorderd Linkedlist");
-
+            //------Ordered Program--------------
             OrderedList orderList = new OrderedList();
-
             Console.WriteLine("Enter word to search");
             string word = Console.ReadLine();
             orderList.Ordered(word);
+
+            //-------UnOrdered Program------------
+            UnorderedList unorderedList = new UnorderedList();
+            Console.WriteLine("Enter A Word ");
+            string word1 = Console.ReadLine();
+            unorderedList.UnOrdered(word);
         }
     }
 }
---
+++ OrderedUnordered/UnorderedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderedUnordered
+{
+    internal class UnorderedList
+    {
+        public Node Head;
+
+        public void UnOrdered(string str)
+        {
+            string text = File.ReadAllText(@"C:\Users\sai\source\repos\RFP-244\OrderedUnordered\OrderedLinkedList\OrderedUnordered\Unordered.txt");
+
+            string[] words = text.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Node node = new Node(words[i]);
+
+                if (this.Head == null)
+                    this.Head = node;
+                else
+
+                {
+                    node.next = this.Head;
+                    this.Head = node;
+
+                }
+
+            }
+            Node temp = this.Head;
+            Node prev = null;
+            int flag = 0;
+
+            while (temp != null)
+            {
+                if (temp.Data.Equals(str))
+                {
+                    prev.next = temp.next;
+                    flag = 1;
+                }
+                prev = temp;
+                temp = temp.next;
+            }
+            if (flag == 0)
+            {
+                Node node = new Node(str);
+                node.next = this.Head;
+                this.Head = node;
+            }
+            Console.WriteLine("List of the searching word");
+            Node node1 = this.Head;
+            string[] array = new string[100];
+            int j = 0;
+            while (node1 != null)
+            {
+                array[j] = node1.Data + "  ";
+                node1 = node1.next;
+                j++;
+            }
+            string concat = string.Concat(array);
+            Console.WriteLine(concat);
+            File.WriteAllText(@"C:\Users\sai\source\repos\RFP-244\OrderedUnordered\OrderedLinkedList\OrderedUnordered\Unordered.txt", concat);
+
+        }
+    }
+}
